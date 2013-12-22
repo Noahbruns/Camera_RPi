2a48dcde7b21e5e853d7a16897d76a8042b545de
Noahbruns <Noah@Noah.local>
2013-12-22 21:16:47 +0100

    Initial Commit

---
+++ Camera_RPi/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Camera_RPi
+//
+//  Created by Noah Bruns on 22.12.13.
+//  Copyright (c) 2013 Noah Bruns. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
