3fe5d7c37754f360358178490b2b5495ce623733
Noahbruns <Noah@noah.home>
2014-01-14 19:10:38 +0100

    Copied src from Pi

--- Camera_RPi/main.cpp
+++ Camera_RPi/main.cpp
@@ -1,92 +1,78 @@
-//Socket_Server
 #include "socket.h"
 #include <string>
 #include <iostream>
+#include <stdlib.h>
 #include <sstream>
-
-#define CAM_IP "192.168.1.100"
-
-using namespace std;
 
 string response;
 string url;
-string x;
-string rec;
-bool state=0;
-bool off=false;
-const string cam=CAM_IP;
+string arg;
 
-bool check_cam(){
-    /*struct sockaddr *sa=;
-    socklen_t len;
-    char hbuf[NI_MAXHOST];
-    
-    if (getnameinfo(sa, len, hbuf, sizeof(hbuf),NULL, 0, NI_NAMEREQD)){
-        printf("could not resolve hostname");
-        return 0;
-    }
-    else {
-        return 1;
-    }*/
-    return 1;
-}
+bool state=true;
+bool run=true;
 
-bool check_tv(){
-    Socket TV;
-    TV.create();
-    TV.connect("192.168.2.2", 80);
-    TV.send("GET /jsonrpc?jsonrpc=2.0&method=\"Player.GetActivePlayers\"&id=1 HTTP/1.1\r\nAuthorization: Basic eGJtYzpyb290\r\nAccept: */*\r\nUser-Agent: curl\r\nContent-Type: application/json\r\nContent-Length: 0\r\n\r\n");
-    TV.recv(rec);
-    cout<<rec;
-    cout.flush();
-    return 1;
-}
+stringstream x;
+string s;
 
 int main(void){
+    sleep(26);
+    system("xbmc-send -a \"PlayMedia(storage/videos/live1.strm)\"");
     Socket sock1;
     sock1.create();
     sock1.bind(5000);
     sock1.listen();
-    while (!off) {
+    while (run) {
         Socket sock2;
         sock1.accept(sock2);
-        string s;
-        stringstream x;
+        x.str("");
         sock2.recv(s);
         x<<s;
-        if (s.at(0)=='G') {
+        url="";
+        getline(x,url);
+        if(url.substr(0,3)=="GET"){
             response="Success";
-            getline(x, url);
-            url=s.substr(4,(s.find(' ',4)-4));
+            url=url.substr(4,url.find(' ',4)-4);
+            if(url.find('?')!=4294967295){
+                arg=url.substr(url.find('?')+1);
+                url=url.substr(0,url.find('?'));
+            }
             
-            if (url.substr(0,3)=="/on") {
-                if(check_cam()){
-                /*if(system("ssh root@192.168.1.102 \"xbmc-send -a 'PlayMedia(storage/videos/live1.strm)'\"")!=0){
+            if (url=="/on") {
+                if(system("xbmc-send -a \"PlayMedia(storage/videos/live1.strm)\"")!=0){
                     response="fail";
-                }*/
+                }
                 state=1;
-                }
-                else {
-                    response="could not connect to CAM";
-                }
             }
-            else if (url.substr(0,4)=="/off") {
-                /*if(system("ssh root@192.168.1.102 \"xbmc-send -a 'PlayerControl(Stop)'\"")!=0){
+            else if (url=="/w?") {
+                string video;
+                video="xbmc-send -a \"PlayMedia(";
+                video.append(s.substr(7,(s.find(' ',7)-7)));
+                video.append(")\"");
+                if(system(video.c_str())!=0){
                     response="fail";
-                }*/
+                }
+                state=1;
+            }
+            else if (url=="/off") {
+                if(system("xbmc-send -a \"PlayerControl(Stop)\"")!=0){
+                    response="fail";
+                }
+                
                 state=0;
             }
-            else if (url.substr(0,9)=="/shutdown") {
-                /*if(system("ssh root@192.168.1.102 \"xbmc-send -a 'PlayerControl(Stop)'\"")!=0){
-                 response="fail";
-                 }*/
+            else if (url=="/shutdown") {
+                if(system("xbmc-send -a \"Shutdown\"")!=0){
+                    response="fail";
+                }
                 state=0;
-                off=true;
             }
-            else if (url.substr(0,5)=="/time"){
+            else if (url=="/exit") {
+                run=false;
+            }
+            else if (url=="/time"){
                 response="don't know :-/";
             }
-            else if (url.substr(0,6)=="/state"){
+            else if (url=="/state"){
                 if (state) {
                     response="ON";
                 }
@@ -96,17 +82,14 @@ int main(void){
             }
             else {
                 response="no Cmd to this url";
-                cout<<"Unknown cmd";
-                cout.flush();
+                //cout<<"Unknown cmd";
+                //cout.flush();
             }
-        
-        sock2.send("HTTP/1.0 200 OK\r\nConnection: close\r\nAccess-Control-Allow-Origin: *\r\nDate: Sat, 28 Nov 2009 04:36:25 GMT\r\nServer: RPi\r\nContent-Type: text/plain\r\n\r\n"+response);
             
-            cout<<"repuest: "<<url <<" => "<<response<<endl;
-            cout.flush();
-        }
-        else {
-            sock2.send("HTTP/1.0 200 OK\r\nConnection: close\r\nAccess-Control-Allow-Origin: *\r\nDate: Sat, 28 Nov 2009 04:36:25 GMT\r\nServer: RPi\r\nContent-Type: text/plain\r\n\r\nNOT A GET METHOD");
+            sock2.send("HTTP/1.0 200 OK\r\nConnection: close\r\nAccess-Control-Allow-Origin: *\r\nDate: Sat, 28 Nov 2009 04:36:25 GMT\r\nServer: RPi\r\nContent-Type: text/html\r\n\r\n"+response);
+            
+            //cout<<"repuest: "<<url <<" => "<<response<<endl;
+            //cout.flush();
         }
     }
     sock1.close();
